dce6e5076484c7f002381a641ef3c2e689434c6d
saroro <hbhj4633@naver.com>
2022-07-14 01:41:38 +0900

    Add class after put stone

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const {Omok} = require("./src/Omok/OmokMain");
+const {Occupied, InvalidPosition, PutError, Forbid33, Forbid44, Forbid6, Forbid, BlackWins, WhiteWins, PutComplete,} = require("./src/Omok/PutResult/putResult");
 module.exports = {
-    "Omok" : Omok
+    "Omok" : Omok,
+    "Occupied" : Occupied,
+    "InvalidPosition" : InvalidPosition,
+    "PutError" : PutError,
+    "Forbid33" : Forbid33,
+    "Forbid44" : Forbid44,
+    "Forbid6" : Forbid6,
+    "Forbid" : Forbid,
+    "BlackWins" : BlackWins,
+    "WhiteWins" : WhiteWins,
+    "PutComplete" : PutComplete,
 }
