6a80e375e0bfb34b6d231f48a2050d862d90ff5e
saroro <hbhj4633@naver.com>
2022-07-15 13:07:42 +0900

    Use IIFE to prevent redeclaration error

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const Omok = require("./src/Omok/OmokMain");
-const Action = require("./src/Omok/ActionClass/Action");
 
+(function(){
+    const Omok = require("./src/Omok/OmokMain");
+    const Action = require("./src/Omok/ActionClass/Action");
+    module.exports = {
+        "Omok" : Omok.Omok,
+        "Occupied" : Action.Occupied,
+        "InvalidPosition" : Action.InvalidPosition,
+        "PutError" : Action.PutError,
+        "Forbid33" : Action.Forbid33,
+        "Forbid44" : Action.Forbid44,
+        "Forbid6" : Action.Forbid6,
+        "Forbid" : Action.Forbid,
+        "BlackWins" : Action.BlackWins,
+        "WhiteWins" : Action.WhiteWins,
+        "PutComplete" : Action.PutComplete,
+        "Undo" : Action.Undo,
+    }
+})();
 
-module.exports = {
-    "Omok" : Omok.Omok,
-    "Occupied" : Action.Occupied,
-    "InvalidPosition" : Action.InvalidPosition,
-    "PutError" : Action.PutError,
-    "Forbid33" : Action.Forbid33,
-    "Forbid44" : Action.Forbid44,
-    "Forbid6" : Action.Forbid6,
-    "Forbid" : Action.Forbid,
-    "BlackWins" : Action.BlackWins,
-    "WhiteWins" : Action.WhiteWins,
-    "PutComplete" : Action.PutComplete,
-    "Undo" : Action.Undo,
-}
